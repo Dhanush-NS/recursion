8de7710ca6f957baa20988ef5e6d4531f7f48dc6
Dhanush-NS <nsdhanush5@gmail.com>
2025-04-12 17:38:15 +0530

    new commit

---
+++ p.py
@@ -0,0 +1,14 @@
+print("Happy no's")
+
+def ishappy(n):
+    if n == 1:
+        return True
+    if n == 4:
+        return False
+    sum = 0
+    while n > 0:
+        rem = n % 10
+        sum += rem**2
+        n = n //10
+    return ishappy(sum)
+print(ishappy(9))
